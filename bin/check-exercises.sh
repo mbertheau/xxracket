89493f7a19af2f602fce5b345cd5013468b53b6c
Markus Bertheau <mbertheau@gmail.com>
2015-05-22 00:07:15 +0200

    Quote correctly

--- bin/check-exercises.sh
+++ bin/check-exercises.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
 for exercise in *; do
-    if [ -d $exercise ]; then
-        cd $exercise
-        if [ -f $exercise-test.rkt ]; then
-            echo Testing $exercise...
-            if [ -f $exercise.rkt ]; then
-                mv $exercise.rkt $exercise.rkt_
+    if [ -d "$exercise" ]; then
+        cd "$exercise" || exit
+        if [ -f "$exercise"-test.rkt ]; then
+            echo Testing "$exercise"...
+            if [ -f "$exercise".rkt ]; then
+                mv "$exercise".rkt "$exercise".rkt_
             fi
-            mv example.rkt $exercise.rkt
-            racket $exercise-test.rkt
+            mv example.rkt "$exercise".rkt
+            racket "$exercise"-test.rkt
             SUCCESS=$?
-            mv $exercise.rkt example.rkt
-            if [ -f $exercise.rkt_ ]; then
-                mv $exercise.rkt_ $exercise.rkt
+            mv "$exercise".rkt example.rkt
+            if [ -f "$exercise".rkt_ ]; then
+                mv "$exercise".rkt_ "$exercise".rkt
             fi
             if [ $SUCCESS -ne 0 ]; then
                 exit $SUCCESS
