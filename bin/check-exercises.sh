8dd8c63c8e50d7a24afbf2412badac07fbc12b1e
Markus Bertheau <mbertheau@gmail.com>
2015-05-22 00:08:35 +0200

    Actually fail travis when a test fails

--- bin/check-exercises.sh
+++ bin/check-exercises.sh
@@ -10,9 +10,13 @@ for exercise in *; do
             fi
             mv example.rkt $exercise.rkt
             racket $exercise-test.rkt
+            SUCCESS=$?
             mv $exercise.rkt example.rkt
             if [ -f $exercise.rkt_ ]; then
                 mv $exercise.rkt_ $exercise.rkt
+            fi
+            if [ $SUCCESS -ne 0 ]; then
+                exit $SUCCESS
             fi
         fi
         cd ..
